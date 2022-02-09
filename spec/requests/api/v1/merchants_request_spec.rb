83a50c033561aa358a1fa29400c57ffad03f655c
Mallory Vining <mallorygallery12@gmail.com>
2022-02-08 17:26:52 -0700

    Add show method

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,15 +8,31 @@ RSpec.describe "Merchant API" do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants["data"].count).to eq(10)
+    expect(merchants[:data].count).to eq(10)
 
     merchants.each do |merchant|
-      expect(merchant.last[0]["attributes"]).to have_key("id")
-      expect(merchant.last[0]["attributes"]["id"]).to be_an(Integer)
-      expect(merchant.last[0]["attributes"]).to have_key("name")
-      expect(merchant.last[0]["attributes"]["name"]).to be_a(String)
+      expect(merchant.last[0][:attributes]).to have_key(:id)
+      expect(merchant.last[0][:attributes][:id]).to be_an(Integer)
+      expect(merchant.last[0][:attributes]).to have_key(:name)
+      expect(merchant.last[0][:attributes][:name]).to be_a(String)
     end
+  end
+
+  it 'gets a merchant by id' do
+    id = create(:merchant).id
+
+    get api_v1_merchant_path(id)
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant[:data][:attributes]).to have_key(:id)
+    expect(merchant[:data][:attributes][:id]).to eq(id)
+
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 end
