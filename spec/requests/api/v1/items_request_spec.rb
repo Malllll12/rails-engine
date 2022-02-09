42ba051f5dc32d691c7a530bbccb1a7a03fa8166
Mallory Vining <mallorygallery12@gmail.com>
2022-02-09 10:00:49 -0700

    Add item show

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Item API" do
-  it "sends a list of all items" do
+  it "gets a list of all items" do
     merchant = create(:merchant)
     create_list(:item, 5, merchant: merchant)
 
@@ -29,6 +29,30 @@ RSpec.describe "Item API" do
       expect(item[1][0][:attributes]).to have_key(:merchant_id)
       expect(item[1][0][:attributes][:merchant_id]).to be_a(Integer)
     end
-
   end
+
+  it 'gets an item' do
+    merchant = create(:merchant)
+    id = merchant.items.create!(name: "Cool thing", description: "It's neat", unit_price: 3.0).id
+
+    get api_v1_item_path(id)
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    # binding.pry
+
+    expect(item[:data][:attributes]).to have_key(:id)
+    expect(item[:data][:attributes][:id]).to eq(id)
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+  end
+
 end
