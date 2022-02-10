1e12598133efaa9664b58f3e9d0355d991023240
Mallory Vining <mallorygallery12@gmail.com>
2022-02-10 12:35:42 -0700

    Add find name method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,10 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.find_name(query)
+    # binding.pry
+    where(['name LIKE ?', "%#{query}%"])
+    .order('name asc')
+    .first
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Merchant do
   it { should have_many(:items)}
+
+  describe 'class methods' do
+    it 'finds merchant by name' do
+      merchant = Merchant.create!(name: "Lil Shop of Horrors")
+      merchant_2 = Merchant.create!(name: "Ruff Crowd Pet Supplies")
+
+      expect(Merchant.find_name("Horrors").name).to eq(merchant.name)
+      expect(Merchant.find_name("Horrors").name).to_not eq(merchant_2.name)
+    end
+  end
 end
