7088902435235f86f6d1f69ee83a83a25c63bb6f
Mallory Vining <mallorygallery12@gmail.com>
2022-02-10 16:45:01 -0700

    Find all items by name

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.find_all_items(query)
+    where(['name ILIKE ?', "%#{query}%"])
+    .order('name asc')
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Item do
   it { should belong_to(:merchant) }
+
+  describe 'class methods' do
+    it "finds all items by search criteria" do
+      merchant = create(:merchant)
+      item_1 = create(:item, name: "Horror Movie", merchant: merchant)
+      item_2 = create(:item, name: "Romantic Movie", merchant: merchant)
+      item_3 = create(:item, name: "Movies and Popcorn", merchant: merchant)
+
+      expect(Item.find_all_items("movie")).to eq([item_1, item_3, item_2])
+    end
+  end
 end
